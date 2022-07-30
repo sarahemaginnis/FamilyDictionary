a568978bb0832b572ce532b0d893cf97dcbca9c7
sarahemaginnis <sarahemaginnis@gmail.com>
2022-07-30 14:03:02 -0500

    Add project files.

---
+++ FamilyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+Dictionary<string, Dictionary<string, string>> myFamily = new();
+myFamily.Add("sister", new() { { "name", "Krista" }, { "age", "42" } });
+myFamily.Add("mother", new() { { "name", "Carol" }, { "age", "70" } });
+myFamily.Add("husband", new() { { "name", "Sean" }, { "age", "36" } });
+myFamily.Add("son", new() { { "name", "Eoghan" }, { "age", "1" } });
+
+foreach (var (familyRelationship, familyAttributes) in myFamily) //touple
+{
+    string familyName = familyAttributes["name"];
+    string familyAge = familyAttributes["age"];
+    Console.WriteLine($"{familyName} is my {familyRelationship} and is {familyAge} years old.");
+}
+
+//Krista is my sister and is 42 years old.
